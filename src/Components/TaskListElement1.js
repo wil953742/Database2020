480b9edbe0be80df94fe537446a5f9ca1b2caa81
wil953742 <wil953742@gmail.com>
2020-11-25 16:24:41 +0900

    Add Tasklistemelement2

--- src/Components/TaskListElement1.js
+++ src/Components/TaskListElement1.js
@@ -2,17 +2,21 @@ import React from "react";
 import styles4 from "../CSS/TaskListElement1Style.module.css";
 
 export const TaskListElement1 = () => {
-  let taskName = "Task Name";
+  let taskName = "Task Name";  
   let taskDesc = "Task Description";
-  let taskSubDate = "Last Submitted Task's Date";
-  let taskFileNum = "The Number of Task Files";
+  let taskSubDate = "20/11/25";
+  let taskFileNum = "3";
 
   return (
     <div className={styles4.container}>
-      <div className={styles4.task_name}>{taskName}</div>
-      <div className={styles4.task_desc}>{taskDesc}</div>
-      <div className={styles4.task_sub_date}>{taskSubDate}</div>
-      <div className={styles4.task_file_num}>{taskFileNum}</div>
+      <div className={`${styles4.task_name} ${styles4.task_list}`}>{taskName}</div>
+      <div className={`${styles4.task_desc} ${styles4.task_list}`}>{taskDesc}</div>
+      <div className={`${styles4.task_sub_date} ${styles4.task_list}`}>{taskSubDate}</div>
+      <div className={`${styles4.task_file_num} ${styles4.task_list}`}>{taskFileNum}</div>
     </div>
   );
 };
+
+
+
+
