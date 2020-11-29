33530e046a0948042b4c6e689b49fbf69a4a5681
KORguy <hyun.kang@hotmail.com>
2020-11-29 21:50:21 +0900

    🙇‍♀️

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 reportWebVitals();
